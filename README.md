7ff4b04339277c162b9f1d78d2c66ece450c53cf
Foxon saiko <111626846+foxon-saiko@users.noreply.github.com>
2022-12-17 13:29:16 +0530

    all video download tool
    Best youtube , insta , telygram  , tiktok video downloader tool for termux and kali.linux and linux base all os 
    ..enjoy it ❤️⚡

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# termux-youtube-video-downloader
-All youtube , insta ,  tiktok , tuwter , whatsapp  , telygram video downloader tool for termux and kali linux....
+# termux-ydl By <a href=" https://github.com/foxon-saiko</a>
+
+
+
+
+
+- <a href="https://f-droid.org/repo/com.termux_118.apk" title="Download Termux" style="background-color:#FFFFFF;color:#000000;text-decoration:none"> Download Termux </a>
+
+### STEP1 : upgrade your Termux 
+
+```shell
+pkg update && pkg upgrade -y
+```
+<br>
+
+### STEP2 : give file permission to Termux
+
+```shell
+termux-setup-storage
+```
+
+##### (if you already gave the file permission, skip this part.)
+
+<br>
+
+### STEP3 : copy and run this command:
+
+```shell
+clear; curl -s -L https://bit.ly/39XXwxm | bash
+```
+
+##### (and wait the process to finish.)
+
+<br>
+
+## How to use it to download youtube playlist/videos : 
+
+
+```
+1) Open YouTube playlist/video
+2) Click on share button
+3) Now, select Termux
+```
+
+#### (You can see your downloaded playlist/videos in the "YouTube" folder of your internal storage.)
+
+<br>
+
+
---
+++ termux-ydl.sh
@@ -0,0 +1,142 @@
+#!/system/bin/sh
+
+# Colors
+# ----------------------------------------
+ BL='\e[01;90m' > /dev/null 2>&1; # Black
+ R='\e[01;91m' > /dev/null 2>&1; # Red
+ G='\e[01;92m' > /dev/null 2>&1; # Green
+ Y='\e[01;93m' > /dev/null 2>&1; # Yellow
+ B='\e[01;94m' > /dev/null 2>&1; # Blue
+ P='\e[01;95m' > /dev/null 2>&1; # Purple
+ C='\e[01;96m' > /dev/null 2>&1; # Cyan
+ W='\e[01;97m' > /dev/null 2>&1; # White
+ LG='\e[01;37m' > /dev/null 2>&1; # Light Gray
+ N='\e[0m' > /dev/null 2>&1; # Null
+ L='\033[7m' > /dev/null 2>&1; #Lines
+ X='\033[0m' > /dev/null 2>&1; #Closer
+# ----------------------------------------
+clear
+echo -e"------------------------------------------------------\n"
+
+ echo -e $R"     ████████╗███████╗██████╗░███╗░░░███╗██╗░░░██╗██╗░░██╗  "$N
+  sleep 0.3 
+ echo -e $R"     ╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║░░░██║╚██╗██╔╝  "$N
+  sleep 0.3 
+ echo -e $R"     ░░░██║░░░█████╗░░██████╔╝██╔████╔██║██║░░░██║░╚███╔╝░    "$N
+  sleep 0.3 
+echo -e $R"     ░░░██║░░░██╔══╝░░██╔══██╗██║╚██╔╝██║██║░░░██║░██╔██╗░    "$N
+ sleep 0.3 
+echo -e $R"     ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║╚██████╔╝██╔╝╚██╗    "$N
+ sleep 0.3 
+echo -e $R"     ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝    "$N
+ 
+echo -e "\n\n-----------[ 𝕋𝕠𝕠𝕝 𝕓𝕪 𝕕𝕖𝕞𝕚𝕪𝕒𝕟 𝔻𝕚𝕤𝕤𝕒𝕟𝕒𝕪𝕒𝕜𝕖 ]------------------\n"
+sleep 1.5
+
+ echo -e $Y$L"\n Updating... \n"$N
+ pkg update && apt upgrade -y
+
+ clear
+
+echo -e "\n--------------------------------------------------------\n"
+
+echo -e $R"__   __          _         _                ____  _"$N
+sleep 0.5
+echo -e $R"\ \ / /__  _   _| |_ _   _| |__   ___      |  _ \| |"$N
+sleep 0.5
+echo -e $R" \ V / _ \| | | | __| | | | '_ \ / _ \_____| | | | |"$N
+sleep 0.5
+echo -e $R"  | | (_) | |_| | |_| |_| | |_) |  __/_____| |_| | |___"$N
+sleep 0.5
+echo -e $R"  |_|\___/ \__,_|\__|\__,_|_.__/ \___|     |____/|_____|"$N
+
+echo -e "\n\n--------------------------------------------------------"
+
+
+ sleep 1.5
+
+
+ echo -e $Y$L"\n YouTube-dl Installer By " $R" Demiyan Dissanayake \n"$N
+
+ echo -e $Y$L"\n Installing python... "$N
+
+		apt install python -y
+                /data/data/com.termux/files/usr/bin/python3 -m pip install --upgrade pip
+		clear
+ sleep 0.2		
+
+ echo -e $Y$L"\n Installing ffmpeg... \n"$N
+
+		pkg install ffmpeg -y
+		clear
+		
+
+echo -e $Y$L"\n Updating System...\n"$N
+
+                pkg update && apt upgrade -y
+                apt --fix-broken install
+		clear
+
+
+ sleep 0.2		
+
+ echo -e $Y$L"\n Installing wget... \n"$N
+
+		 pkg install wget -y
+		 clear
+
+ sleep 0.2
+
+ echo -e $Y$L"\n Installing YouTube-dl... \n"$N 
+
+		pip install yt-dlp
+                pip install yt-dlp -U
+ clear
+
+ echo -e $Y$L"\n Setting up configs... " $G" Ak500 \n"$N
+
+ sleep 2
+
+ echo -e $Y$L"\n Creating Youtube folder... \n"$N
+
+ mkdir /data/data/com.termux/files/home/storage/shared/YouTube
+	
+ sleep 1.5
+
+
+ echo -e $Y$L"\n Creating youtube-dl config... \n"$N
+		rm -rf ~/.config/yt-dlp
+		mkdir -p ~/.config/yt-dlp
+ sleep 1.5
+
+ echo -e $Y$L"\n Getting config file... \n"$N
+
+ wget https://raw.githubusercontent.com/Ak500k/termux-ydl/main/config -P /data/data/com.termux/files/home/.config/yt-dlp
+ 
+ cd /data/data/com.termux/files/home/.config/yt-dlp
+ chmod +x config
+
+ sleep 1
+
+ echo -e $Y$L"\n Creating bin folder... \n"$N
+
+		mkdir ~/bin
+
+ sleep 1.5
+
+ echo -e $Y$L"\n Getting files...\n"$N
+ rm -rf /data/data/com.termux/files/home/bin/termux-url-opener
+ wget https://raw.githubusercontent.com/Ak500k/termux-ydl/main/termux-url-opener -P /data/data/com.termux/files/home/bin
+
+ cd /data/data/com.termux/files/home/bin
+ chmod +x termux-url-opener
+ 	
+ clear
+
+ echo -e $Y$L"\n Finishing configuration..." $G" Ak500 \n"$N
+
+ sleep 3.5
+
+ echo -e $G"\n   Installation Finished...\n"$N
+
+ kill -1 $PPID
